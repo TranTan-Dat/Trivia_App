20f15cab48d9afa9aada42c1a8e4709fcfa5194c
Tran Tan Dat <trand6930@gmail.com>
2024-10-30 19:05:50 +0700

    Add test cases and configure sensitive data handling.

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
 
-database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+load_dotenv()
+
+database_name = os.getenv('DB_NAME')
+database_path = 'postgresql://{}/{}'.format(os.getenv('DB_PATH'), database_name)
 
 db = SQLAlchemy()
 
